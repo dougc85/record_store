4dc32291a59372fed1e80f6200c2dbedeeb596f3
Doug Carter <dougc85@gmail.com>
2022-02-15 14:22:53 -0800

    home screen first pass done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.scss';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className="Home">
-
+      <h1 className="Home-title"><span className="Home-title-top"><span className="blue">Blue</span> Note Bin</span>A jazz record store</h1>
+      <Link to="/shop">Visit the Shop</Link>
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Navbar.scss";
+import { Link, Outlet } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className="Navbar">
-      Bloop
+    <div className="App-container">
+      <nav className="Navbar">
+        <ul>
+          <li className="Navbar-item logo">
+            <Link to="/"><span className="blue">Blue</span> Note Bin</Link>
+          </li>
+          <li className="Navbar-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="Navbar-item">
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li className="Navbar-item">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="Navbar-item">
+            <Link to="cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
     </div>
   )
 }
